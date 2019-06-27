78d274a86de4e0ac6f94375585a161dcc8221134
Giuliano Machado <giufma@gmail.com>
2019-06-27 11:54:21 -0300

    Updates

---
+++ precache-manifest.f28c50f61449b032c29379443b05d59f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7826d5d31b422c9f88fa2b83677a88b7",
+    "url": "/givliano/pomodoro-timer/index.html"
+  },
+  {
+    "revision": "48e0e33ecb84c19d25f1",
+    "url": "/givliano/pomodoro-timer/static/css/main.1fea989a.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/givliano/pomodoro-timer/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "48e0e33ecb84c19d25f1",
+    "url": "/givliano/pomodoro-timer/static/js/main.d6772e35.chunk.js"
+  },
+  {
+    "revision": "9f9a7d257526ca528a8a",
+    "url": "/givliano/pomodoro-timer/static/js/runtime~main.e66152d0.js"
+  }
+]);
